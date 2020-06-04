a8ba6c996714baa5302aa076cb161de1a83274d1
Higor Santos <higoraugusto120@gmail.com>
2020-06-04 11:34:24 -0300

    subindo projeto

---
+++ Academico/Controllers/InstituicaoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> Instituicoes = new List<Isti>
+        new InstituicaoController()
+        {
+            InstituicaoID = 1,
+            Nome = "Hogwarts",
+            Endereco ="Escola"
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
